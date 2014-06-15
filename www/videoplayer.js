5af39237969de94f0a0b844aa6d15c20d8fb22ac
mike read <micahelread@ihopkc.org>
2014-06-14 22:57:37 -0500

    updated js parts of plugin

--- www/videoplayer.js
+++ www/videoplayer.js
@@ -1,5 +1,7 @@
-window.play = function(str, callback) {
+var play = function(str, callback) {
     cordova.exec(callback, function(err) {
-        callback('Nothing to echo.');
+        alert('Nothing to echo.');
     }, "videoplugin", "play", [str]);
 }
+
+module.exports = play;
